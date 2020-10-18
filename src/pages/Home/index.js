4c2c2fb0f3cbd70c86dbf545899dd9286a65df5e
zubair-ashraf <zubair8767844@gmail.com>
2020-10-18 22:57:47 +0500

    add redirect file

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,10 +5,10 @@ import { Sidebar, ConversationPanel } from "components";
 function Home() {
   return (
     <Row className="vh-100 m-0">
-      <Col md={3} className="bg-secondary p-0 border-right-gray">
+      <Col md={3} className="sidebar bg-secondary p-0 border-right-gray">
         <Sidebar />
       </Col>
-      <Col md={9} className="bg-dark">
+      <Col md={9} className="bg-dark right-panel">
         <ConversationPanel />
       </Col>
     </Row>
